4ece1e5c450193543ebc18cf23d4d884d57b81cd
ucha <uchaucha419@gmail.com>
2023-08-17 00:52:59 +0400

    added password validation in register page

--- frontend/src/pages/register/RegisterPage.tsx
+++ frontend/src/pages/register/RegisterPage.tsx
@@ -5,16 +5,27 @@ import openviduApi from "../../api/openvidu";
 const MainPage = () => {
     const [username, setUsername] = useState<string>('');
     const [password, setPassword] = useState<string>('');
+    const [repeatedPassword, setRepeatedPassword] = useState<string>('');
+    const [incorrectPassword, setIncorrectPassword] = useState<boolean>(false);
 
     const navigate = useNavigate();
 
     const onCreate = async () => {
-        const sessionId = await openviduApi.createSession();
-        navigate(`/room/${sessionId}`, { state: { username: username } });
+        if(password == repeatedPassword){
+            const sessionId = await openviduApi.createSession();
+            navigate(`/room/${sessionId}`, { state: { username: username } });
+        }else{
+            setIncorrectPassword(true);
+        }
     }
+
+     const getMaskedPassword = (input: string) => {
+        return input.replace(/./g, "*");
+    };
 
     const handleChangeUserName = (e: React.ChangeEvent<HTMLInputElement>) => setUsername(e.target.value);
     const handleChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value);
+     const handleChangeRepeatedPassword = (e: React.ChangeEvent<HTMLInputElement>) => setRepeatedPassword(e.target.value);
 
     return (
         <div>
@@ -38,25 +49,26 @@ const MainPage = () => {
                             className="form-control"
                             type="text"
                             id="userName"
-                            value={password}
+                            value={getMaskedPassword(password)}
                             onChange={handleChangePassword}
                             required
                         />
                     </p>
                     <p>
-                        <label>RepeatPassword: </label>
+                        <label>Repeat Password: </label>
                         <input
                             className="form-control"
                             type="text"
                             id="userName"
-                            value={password}
-                            onChange={handleChangePassword}
+                            value={getMaskedPassword(repeatedPassword)}
+                            onChange={handleChangeRepeatedPassword}
                             required
                         />
                     </p>
                     <p className="text-center">
                         <button className="btn btn-lg btn-success" onClick={onCreate}> CREATE </button>
                     </p>
+                    {incorrectPassword && <h4> Passwords do not match</h4>}
                 </div>
             </div>
         </div>
