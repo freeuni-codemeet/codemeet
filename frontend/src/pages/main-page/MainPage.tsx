eed6dc6dee03570456c4dad842104c6e6197dc7c
Akaki Zghuladze <zghuladze16@gmail.com>
2023-08-20 19:03:01 +0400

    Removed redundant white mode styles

--- frontend/src/pages/main-page/MainPage.tsx
+++ frontend/src/pages/main-page/MainPage.tsx
@@ -29,7 +29,7 @@ const MainPage = () => {
               <input
                 type="text"
                 id="username"
-                className="block px-2.5 pb-2.5 pt-4 w-full text-lg text-gray-900 bg-transparent rounded-lg border border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-sky-700 focus:outline-none focus:ring-0 focus:border-sky-700 peer"
+                className="block px-2.5 pb-2.5 pt-4 w-full text-lg bg-transparent rounded-lg border appearance-none text-white border-gray-600 focus:outline-none focus:ring-0 focus:border-sky-700 peer"
                 placeholder=" "
                 value={username}
                 onChange={handleChangeUserName}
@@ -37,7 +37,7 @@ const MainPage = () => {
               />
               <label
                 htmlFor="username"
-                className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-sky-700 peer-focus:dark:text-sky-700 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
+                className="absolute text-sm text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-gray-900 px-2 peer-focus:px- peer-focus:text-sky-700 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
               >
                 Nickname
               </label>
