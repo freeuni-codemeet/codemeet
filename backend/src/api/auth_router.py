1e30fcd64d079d6a57c9391810554011261f26aa
ucha <uchaucha419@gmail.com>
2023-08-23 00:10:08 +0400

    minor changes based on pr comments

--- backend/src/api/auth_router.py
+++ backend/src/api/auth_router.py
@@ -13,7 +13,7 @@ def add_user(user: user.UserCreate, auth_service: AuthService = Depends()):
     return auth_service.add_user(user)
 
 
-@auth_router.get("/getUser")
+@auth_router.get("/{username_or_email}")
 @inject
 def get_user(usernameOrEmail: str, auth_service: AuthService = Depends()):
     return auth_service.get_user(usernameOrEmail)
