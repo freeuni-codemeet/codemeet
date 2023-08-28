2c0e127b9208ea03c275d9a297cfc15e31120059
ucha <uchaucha419@gmail.com>
2023-08-28 21:13:02 +0400

    added compiler button and languages in room page

--- frontend/src/pages/room/Room.tsx
+++ frontend/src/pages/room/Room.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
 import UserVideoComponent from "./UserVideoComponent";
 import useVideoChat from "../../hooks/useVideoChat";
 import CodeEditor from "./CodeEditor";
@@ -11,6 +12,7 @@ const Room = () => {
 
   const [videoEnabled, setVideoEnabled] = useState<boolean>(true);
   const [audioEnabled, setAudioEnabled] = useState<boolean>(true);
+  const [selectedLanguage, setSelectedLanguage] = useState("python"); // Default language is "python"
 
   const usernameRef: React.MutableRefObject<string> = useRef(
     state?.username || `participant${Math.random() * 100 + 1}`
@@ -39,6 +41,24 @@ const Room = () => {
     const enabled = !audioEnabled;
     publisher?.publishAudio(enabled);
     setAudioEnabled(enabled);
+  };
+
+  const handleLanguageChange = (event) => {
+    setSelectedLanguage(event.target.value);
+  };
+
+  const handleCompileClick = async () => {
+    try {
+      const response = await axios.get("/core-api/compiler/compile", {
+        params: {
+          language: selectedLanguage,
+          code: "code_from_editor", // Replace with your actual code from the editor
+        },
+      });
+      console.log("Compilation result:", response.data);
+    } catch (error) {
+      console.error("Error compiling:", error);
+    }
   };
 
   return (
@@ -71,6 +91,13 @@ const Room = () => {
                 onClick={handleLeaveSessionClicked}
                 value="Leave session"
               />
+              <button onClick={handleCompileClick}>Compile</button>
+              <select value={selectedLanguage} onChange={handleLanguageChange}>
+                <option value="python">Python</option>
+                <option value="rust">Rust</option>
+                <option value="cpp">C++</option>
+                <option value="c">C</option>
+              </select>
             </div>
 
             <div>
