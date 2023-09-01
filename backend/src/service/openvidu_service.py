08437d7682e1f46d6508ab55c928b2f14e2dbd08
Akaki Zghuladze <zghuladze16@gmail.com>
2023-09-01 22:25:28 +0400

    Added sessionId in JWT token

--- backend/src/service/openvidu_service.py
+++ backend/src/service/openvidu_service.py
@@ -22,31 +22,34 @@ class OpenviduService:
     def __init__(self, openvidu_connector: OpenviduConnector):
         self.openvidu_connector = openvidu_connector
 
-    def _signJwt(self, openviduRole: OpenviduRole) -> str:
+    def _signJwt(self, session_id: str, openviduRole: OpenviduRole) -> str:
         payload = {
+            "sessionId": session_id,
             "permission": openviduRole.name,
-            "expiry": time.time() + 600
+            "expiry": time.time() + 30
         }
         token = jwt.encode(payload=payload, key=JWT_SECRET, algorithm=JWT_ALGORITHM)
         return token
 
-    def _verifyJwt(self, token: str) -> bool:
+    def _verifyJwt(self, session_id: str, token: str | None) -> bool:
+        if token is None:
+            return False
         decoded = jwt.decode(jwt=token, key=JWT_SECRET, algorithm=JWT_ALGORITHM)
-        return decoded.get("permission") == OpenviduRole.MODERATOR.name  # TODO check expiry
+        return decoded.get("permission") == OpenviduRole.MODERATOR.name and decoded.get("sessionId") == session_id  # TODO check expiry
 
     async def create_session(self) -> CreateSessionResponse:
         session_id = str(uuid.uuid4())
         response = await self.openvidu_connector.create_session(session_id)
         response.raise_for_status()
         response_json = response.json()
-        return CreateSessionResponse(str(response_json["sessionId"]), self._signJwt(OpenviduRole.MODERATOR))
+        return CreateSessionResponse(str(response_json["sessionId"]), self._signJwt(session_id=session_id, openviduRole=OpenviduRole.MODERATOR))
 
     async def create_connection(
             self,
             session_id: str,
             secret_token: str | None
     ) -> str:
-        is_moderator = secret_token is not None and self._verifyJwt(secret_token)
+        is_moderator = secret_token is not None and self._verifyJwt(session_id=session_id, token=secret_token)
         response = await self.openvidu_connector.create_connection(session_id, OpenviduRole.MODERATOR if is_moderator else OpenviduRole.PUBLISHER)
         if response.status_code == 404:
             raise SessionNotFoundException(f"Session with id {session_id} not found")
